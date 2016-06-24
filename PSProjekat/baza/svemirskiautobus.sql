7aee710b1f4a2336bcc5cea6ae5dbd98862784b0
hachiko93 <ana_93_bg@hotmail.com>
2016-06-24 17:51:51 +0200

    dodata baza i pokusan build
    Build nece (???)

--- PSProjekat/baza/svemirskiautobus.sql
+++ PSProjekat/baza/svemirskiautobus.sql
@@ -26,11 +26,11 @@ CREATE TABLE `planeta` (
   `naziv` varchar(20) NOT NULL,
   `udaljenostOdZemlje` double NOT NULL,
   PRIMARY KEY (`planetaID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `planeta` */
 
-insert  into `planeta`(`planetaID`,`koordinate`,`naziv`,`udaljenostOdZemlje`) values (2,'5869','Jupiter',5220);
+insert  into `planeta`(`planetaID`,`koordinate`,`naziv`,`udaljenostOdZemlje`) values (2,'5869','Jupiter',5220),(3,'10.086','Saturn',7.45),(4,'10.086','Uran',7.45),(5,'20.64','Neptun',10.05),(6,'6.02','Jupiter',3.3);
 
 /*Table structure for table `porudzbenica` */
 
@@ -49,6 +49,8 @@ CREATE TABLE `porudzbenica` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `porudzbenica` */
+
+insert  into `porudzbenica`(`porudzbenicaID`,`autobusID`,`planetaID`,`datum`) values ('SIF-1-2-09-05-16',1,2,'2016-05-09'),('SIF-1-2-10-05-16',1,2,'2016-05-10'),('SIF-1-2-20-06-16',1,2,'2016-06-20');
 
 /*Table structure for table `profaktura` */
 
@@ -72,6 +74,8 @@ CREATE TABLE `profaktura` (
 
 /*Data for the table `profaktura` */
 
+insert  into `profaktura`(`profakturaID`,`putnikID`,`autobusID`,`planetaID`,`iznos`,`datum`) values ('SIF-4-2-1-09-05-16',4,1,2,261,'2016-05-09'),('SIF-4-2-1-30-06-16',4,1,2,261,'2016-06-30');
+
 /*Table structure for table `putnik` */
 
 DROP TABLE IF EXISTS `putnik`;
@@ -83,11 +87,11 @@ CREATE TABLE `putnik` (
   `adresa` varchar(50) NOT NULL,
   `kontaktTelefon` varchar(20) NOT NULL,
   PRIMARY KEY (`putnikID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `putnik` */
 
-insert  into `putnik`(`putnikID`,`jmbg`,`imeprezime`,`adresa`,`kontaktTelefon`) values (1,'2708993715271','Ana Licina','Slavonskih brigada 45','0656268109'),(2,'1808996715141','Jovana Jovanovic','Jove Ilica 58','061525252');
+insert  into `putnik`(`putnikID`,`jmbg`,`imeprezime`,`adresa`,`kontaktTelefon`) values (4,'2708993715271','Ana Licina','Slavonskih brigada 45','0656268109'),(5,'1111111111111','Ana Licina','Takovska 67','065111112'),(6,'1111111111112','Ana Licina','Kosovska 5','069555555'),(7,'1210993715454','Ivana Kalender','Veljka Petrovica 57','063363611');
 
 /*Table structure for table `stavkaporudzbenice` */
 
@@ -108,6 +112,8 @@ CREATE TABLE `stavkaporudzbenice` (
 
 /*Data for the table `stavkaporudzbenice` */
 
+insert  into `stavkaporudzbenice`(`rb`,`porudzbenicaID`,`zaliheID`,`kolicina`,`jedinicaMere`) values (1,'SIF-1-2-09-05-16',1,40,'kom'),(1,'SIF-1-2-20-06-16',1,10,'kom'),(2,'SIF-1-2-09-05-16',2,20,'kom'),(2,'SIF-1-2-20-06-16',2,20,'kom');
+
 /*Table structure for table `svemirskiautobus` */
 
 DROP TABLE IF EXISTS `svemirskiautobus`;
@@ -117,11 +123,11 @@ CREATE TABLE `svemirskiautobus` (
   `naziv` varchar(20) NOT NULL,
   `maksimalanbrojsedista` int(11) NOT NULL,
   PRIMARY KEY (`autobusID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `svemirskiautobus` */
 
-insert  into `svemirskiautobus`(`autobusID`,`naziv`,`maksimalanbrojsedista`) values (1,'Bambus',100);
+insert  into `svemirskiautobus`(`autobusID`,`naziv`,`maksimalanbrojsedista`) values (1,'Bambus',100),(2,'Lasta',300),(3,'Ikarbus',400),(4,'Sajberbus',1000),(5,'Krilati orao',150);
 
 /*Table structure for table `zalihe` */
 
@@ -133,11 +139,11 @@ CREATE TABLE `zalihe` (
   `naziv` varchar(50) NOT NULL,
   `stanje` double NOT NULL,
   PRIMARY KEY (`zaliheID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `zalihe` */
 
-insert  into `zalihe`(`zaliheID`,`tip`,`naziv`,`stanje`) values (1,'hrana','Sendvic',50),(2,'hrana','Banana',20);
+insert  into `zalihe`(`zaliheID`,`tip`,`naziv`,`stanje`) values (1,'hrana','Sendvic',50),(2,'hrana','Banana',20),(3,'gorivo','Petrol',40),(4,'gas','Kiseonik',60),(5,'lek','Kafetin',50);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
